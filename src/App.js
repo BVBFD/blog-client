a9c07935689f93cb3d6e4943abcb8d028ce290c7
BVBFD <lsevina126@gmail.com>
2023-01-27 08:31:50 +0900

    edit redirect url

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import React, { useEffect, useState } from "react";
-import { Route, Routes } from "react-router-dom";
-import About from "./pages/about/About";
-import Contact from "./pages/contact/Contact";
-import Home from "./pages/home/Home";
-import Login from "./pages/login/Login";
-import Post from "./pages/post/Post";
-import Setting from "./pages/setting/Setting";
-import Signup from "./pages/signup/Signup";
-import Write from "./pages/write/Write";
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import About from './pages/about/About';
+import Contact from './pages/contact/Contact';
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import Post from './pages/post/Post';
+import Setting from './pages/setting/Setting';
+import Signup from './pages/signup/Signup';
+import Write from './pages/write/Write';
 
 const App = (props) => {
   return (
     <Routes>
-      <Route path={"/"} element={<Home />} />
-      <Route path={"/login"} element={<Login />} />
-      <Route path={"/signup"} element={<Signup />} />
-      <Route path={"/write"} element={<Write />} />
-      <Route path={"/about"} element={<About />} />
-      <Route path={"/contact"} element={<Contact />} />
-      <Route path={"/setting"} element={<Setting />} />
-      <Route path={"/post/:id"} element={<Post />} />
+      <Route path={'/'} element={<Home />} />
+      <Route path={'/login'} element={<Login />} />
+      <Route path={'/signup'} element={<Signup />} />
+      <Route path={'/write'} element={<Write />} />
+      <Route path={'/about'} element={<About />} />
+      <Route path={'/contact'} element={<Contact />} />
+      <Route path={'/setting'} element={<Setting />} />
+      <Route path={'/post/:id'} element={<Post />} />
     </Routes>
   );
 };
